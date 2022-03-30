434af7212ea9e53d8c753bb12ec0d20faf40540a
runil <57109651+runil@users.noreply.github.com>
2022-03-30 10:22:35 +0200

    setup: Setup iOS Swift project in XCode

---
+++ Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobile
+//
+//  Created by user on 2022-03-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
