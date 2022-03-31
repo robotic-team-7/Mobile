650146d8dae4cc11a5d78cc7c28afd2e72054461
runil <57109651+runil@users.noreply.github.com>
2022-03-31 15:08:46 +0200

    setup: Setup iOS Swift project in XCode (#2)

---
+++ Mobile/MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp.swift
+//  Mobile
+//
+//  Created by user on 2022-03-30.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
