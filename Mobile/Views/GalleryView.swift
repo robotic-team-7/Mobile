ad172a7ca0b84466db96a7e76e7e1b5b70ec74da
Filip Flodén <48211957+filipfloden@users.noreply.github.com>
2022-04-14 13:56:15 +0200

    feat(gallery): added a clear gallery button

--- Mobile/Views/GalleryView.swift
+++ Mobile/Views/GalleryView.swift
@@ -8,32 +8,45 @@
 import SwiftUI
 
 struct GalleryView: View {
-    let data = ["cat", "dog", "fox", "koala", "squirrel"]
+    private let data = ["cat", "dog", "fox", "koala", "squirrel"]
 
-    let columns = [
+    private let columns = [
         GridItem(.flexible(), spacing: 15),
         GridItem(.flexible(), spacing: 15)
     ]
+    
+    let screenWidth = UIScreen.main.bounds.size.width
     
     var body: some View {
         ZStack {
             Color.scheme.bg
                 .ignoresSafeArea()
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(data, id: \.self) { item in
-                        ZStack {
-                            Image(item)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(minWidth: 0, maxWidth: .infinity)
-                                .aspectRatio(1, contentMode: .fit)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 2))
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(data, id: \.self) { item in
+                            ZStack {
+                                Image(item)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(minWidth: 0, maxWidth: .infinity)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 2))
+                            }
                         }
                     }
-                }.padding(.horizontal)
-            }
+                }.padding()
+                Button(action: {
+                    print("Clear gallery")
+                }) {
+                    Text("Clear")
+                        .frame(maxWidth: screenWidth * 0.5)
+                        .frame(height: 48)
+                }
+                .buttonStyle(.bordered)
+                .disabled(false)
+            }.padding(.horizontal)
         }
     }
 }
