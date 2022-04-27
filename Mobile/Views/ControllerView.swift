a4a304a99d31ef5cdc9f835b89c8806e086e1774
runil <57109651+runil@users.noreply.github.com>
2022-04-27 19:35:45 +0200

    feat(bluetoothapi): Added bluetooth connection condition for accessing D-Pad

--- Mobile/Views/ControllerView.swift
+++ Mobile/Views/ControllerView.swift
@@ -13,83 +13,92 @@ struct ControllerView: View {
         ZStack {
             Color.scheme.bg
                 .ignoresSafeArea()
-            VStack() {
-                Spacer()
-                HStack {
-                    Button(action: {
-                    }) {
-                        Image(systemName: "camera.fill")
-                            .font(.largeTitle)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 48)
-                    }
-                    .buttonStyle(.bordered)
-                    Button(action: {
-                    }) {
-                        Image(systemName: "power")
-                            .font(.largeTitle)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 48)
-                    }
-                    .buttonStyle(.bordered)
-                }
-                Spacer()
-                HStack {
+            if bleDelegate.connected && bleDelegate.loaded {
+                VStack() {
                     Spacer()
-                    Button(action: {
-                        bleDelegate.sendCommand(command: 3)
-                    }) {
-                        Image(systemName: "arrowtriangle.up.fill")
-                            .font(.largeTitle)
-                            .frame(width: 48, height: 48)
+                    HStack {
+                        Button(action: {
+                        }) {
+                            Image(systemName: "camera.fill")
+                                .font(.largeTitle)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 48)
+                        }
+                        .buttonStyle(.bordered)
+                        Button(action: {
+                        }) {
+                            Image(systemName: "power")
+                                .font(.largeTitle)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 48)
+                        }
+                        .buttonStyle(.bordered)
                     }
-                    .buttonStyle(.bordered)
                     Spacer()
-                }
-                HStack(alignment:.center) {
-                    Button(action: {
-                        bleDelegate.sendCommand(command: 1)
-                    }) {
-                        Image(systemName: "arrowtriangle.left.fill")
-                            .font(.largeTitle)
-                            .frame(width: 48, height: 48)
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            bleDelegate.sendCommand(command: 3)
+                        }) {
+                            Image(systemName: "arrowtriangle.up.fill")
+                                .font(.largeTitle)
+                                .frame(width: 48, height: 48)
+                        }
+                        .buttonStyle(.bordered)
+                        Spacer()
+                    }
+                    HStack(alignment:.center) {
+                        Button(action: {
+                            bleDelegate.sendCommand(command: 1)
+                        }) {
+                            Image(systemName: "arrowtriangle.left.fill")
+                                .font(.largeTitle)
+                                .frame(width: 48, height: 48)
 
-                    }
-                    .buttonStyle(.bordered)
-                    //Spacer()
-                    Button(action: {
-                    }) {
-                        Image(systemName: "")
-                            .font(.largeTitle)
-                            .frame(width: 48, height: 48)
+                        }
+                        .buttonStyle(.bordered)
+                        //Spacer()
+                        Button(action: {
+                        }) {
+                            Image(systemName: "")
+                                .font(.largeTitle)
+                                .frame(width: 48, height: 48)
 
+                        }
+                        .buttonStyle(.bordered)
+                        .hidden()
+                        Button(action: {
+                            bleDelegate.sendCommand(command: 4)
+                        }) {
+                            Image(systemName: "arrowtriangle.right.fill")
+                                .font(.largeTitle)
+                                .frame(width: 48, height: 48)
+                        }
+                        .buttonStyle(.bordered)
                     }
-                    .buttonStyle(.bordered)
-                    .hidden()
-                    Button(action: {
-                        bleDelegate.sendCommand(command: 4)
-                    }) {
-                        Image(systemName: "arrowtriangle.right.fill")
-                            .font(.largeTitle)
-                            .frame(width: 48, height: 48)
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            bleDelegate.sendCommand(command: 2)
+                        }) {
+                            Image(systemName: "arrowtriangle.down.fill")
+                                .font(.largeTitle)
+                                .frame(width: 48, height: 48)
+                            
+                        }
+                        .buttonStyle(.bordered)
+                        Spacer()
                     }
-                    .buttonStyle(.bordered)
-                }
-                HStack {
                     Spacer()
-                    Button(action: {
-                        bleDelegate.sendCommand(command: 2)
-                    }) {
-                        Image(systemName: "arrowtriangle.down.fill")
-                            .font(.largeTitle)
-                            .frame(width: 48, height: 48)
-                        
-                    }
-                    .buttonStyle(.bordered)
-                    Spacer()
-                }
-                Spacer()
-            }.padding()
+                }.padding()
+            }
+            else {
+                VStack {
+                    Text(bleDelegate.connected ? "Loading device data..." : "Searching for device...")
+                    ProgressView()
+                }.padding()
+            }
+            
         }
     }
 }
