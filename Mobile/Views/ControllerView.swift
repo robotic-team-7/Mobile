3a33c209b3ab807a1d439933261af565ff9350ea
runil <57109651+runil@users.noreply.github.com>
2022-05-02 13:15:00 +0200

    feat(bluetoothapi): Defined view as navigationitem to resolve issue with navigationbar title

--- Mobile/Views/ControllerView.swift
+++ Mobile/Views/ControllerView.swift
@@ -15,138 +15,140 @@ struct ControllerView: View {
         UITableViewCell.appearance().backgroundColor = .clear // cell background
     }
     var body: some View {
-        ZStack {
-            Color.scheme.bg
-                .ignoresSafeArea()
-            if bleManager.isDisconnected {
-                VStack {
-                    List(bleManager.peripherals) { peripheral in Button(action: {deviceSelected = true; bleManager.connectToPeripheral(peripheral: peripheral.object)}){
+        NavigationView {
+            ZStack {
+                Color.scheme.bg
+                    .ignoresSafeArea()
+                if bleManager.isDisconnected {
+                    VStack {
+                        List(bleManager.peripherals) { peripheral in Button(action: {deviceSelected = true; bleManager.connectToPeripheral(peripheral: peripheral.object)}){
+                            HStack {
+                                Text(peripheral.name)
+                                Spacer()
+                                Text(String(peripheral.rssi))
+                            }
+                        }.listRowBackground(Color.scheme.darkBg)}
+                        Spacer()
+                        Text("Status")
+                        if bleManager.isSwitchedOn {
+                            Text("Bluetooth is switched on")
+                                .foregroundColor(.green)
+                        }
+                        else {
+                            Text("Bluetooth is switched off")
+                                .foregroundColor(.red)
+                        }
                         HStack {
-                            Text(peripheral.name)
+                            Button(action: {
+                                bleManager.startScanning()
+                            }) {
+                                Text("Scan")
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 48)
+                            }
+                            Button(action: {
+                                bleManager.disconnectPeripheral()
+                            }) {
+                                Text("Disconnect")
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 48)
+                            }.disabled(bleManager.isDisconnected == true)
+                        }
+                    }.padding()
+                }
+                else {
+                    VStack() {
+                        Spacer()
+                        HStack {
+                            Button(action: {
+                                bleManager.writeOutgoingValue(data: "SD")
+                            }) {
+                                Image(systemName: "tortoise.fill")
+                                    .font(.largeTitle)
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 48)
+                            }
+                            .buttonStyle(.bordered)
+                            Button(action: {
+                                bleManager.writeOutgoingValue(data: "MS")
+                            }) {
+                                Image(systemName: "power")
+                                    .font(.largeTitle)
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 48)
+                            }
+                            .buttonStyle(.bordered)
+                            Button(action: {
+                                bleManager.writeOutgoingValue(data: "SI")
+                            }) {
+                                Image(systemName: "hare.fill")
+                                    .font(.largeTitle)
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 48)
+                            }
+                            .buttonStyle(.bordered)
+                        }
+                        Spacer()
+                        HStack {
                             Spacer()
-                            Text(String(peripheral.rssi))
+                            Button(action: {
+                                bleManager.writeOutgoingValue(data: "MF")
+                            }) {
+                                Image(systemName: "arrowtriangle.up.fill")
+                                    .font(.largeTitle)
+                                    .frame(width: 48, height: 48)
+                            }
+                            .buttonStyle(.bordered)
+                            Spacer()
                         }
-                    }.listRowBackground(Color.scheme.darkBg)}
-                    Spacer()
-                    Text("Status")
-                    if bleManager.isSwitchedOn {
-                        Text("Bluetooth is switched on")
-                            .foregroundColor(.green)
-                    }
-                    else {
-                        Text("Bluetooth is switched off")
-                            .foregroundColor(.red)
-                    }
-                    HStack {
-                        Button(action: {
-                            bleManager.startScanning()
-                        }) {
-                            Text("Scan")
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 48)
+                        HStack(alignment:.center) {
+                            Button(action: {
+                                bleManager.writeOutgoingValue(data: "ML")
+                            }) {
+                                Image(systemName: "arrowtriangle.left.fill")
+                                    .font(.largeTitle)
+                                    .frame(width: 48, height: 48)
+                            }
+                            .buttonStyle(.bordered)
+                            //Spacer()
+                            Button(action: {
+                            }) {
+                                Image(systemName: "")
+                                    .font(.largeTitle)
+                                    .frame(width: 48, height: 48)
+                                
+                            }
+                            .buttonStyle(.bordered)
+                            .hidden()
+                            Button(action: {
+                                bleManager.writeOutgoingValue(data: "MR")
+                            }) {
+                                Image(systemName: "arrowtriangle.right.fill")
+                                    .font(.largeTitle)
+                                    .frame(width: 48, height: 48)
+                            }
+                            .buttonStyle(.bordered)
                         }
-                        Button(action: {
-                            bleManager.disconnectPeripheral()
-                        }) {
-                            Text("Disconnect")
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 48)
-                        }.disabled(bleManager.isDisconnected == true)
-                    }
-                }.padding()
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                bleManager.writeOutgoingValue(data: "MB")
+                            }) {
+                                Image(systemName: "arrowtriangle.down.fill")
+                                    .font(.largeTitle)
+                                    .frame(width: 48, height: 48)
+                                
+                            }
+                            .buttonStyle(.bordered)
+                            Spacer()
+                        }
+                        Spacer()
+                    }.padding()
+                }
             }
-            else {
-                VStack() {
-                    Spacer()
-                    HStack {
-                        Button(action: {
-                            bleManager.writeOutgoingValue(data: "SD")
-                        }) {
-                            Image(systemName: "tortoise.fill")
-                                .font(.largeTitle)
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 48)
-                        }
-                        .buttonStyle(.bordered)
-                        Button(action: {
-                            bleManager.writeOutgoingValue(data: "MS")
-                        }) {
-                            Image(systemName: "power")
-                                .font(.largeTitle)
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 48)
-                        }
-                        .buttonStyle(.bordered)
-                        Button(action: {
-                            bleManager.writeOutgoingValue(data: "SI")
-                        }) {
-                            Image(systemName: "hare.fill")
-                                .font(.largeTitle)
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 48)
-                        }
-                        .buttonStyle(.bordered)
-                    }
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            bleManager.writeOutgoingValue(data: "MF")
-                        }) {
-                            Image(systemName: "arrowtriangle.up.fill")
-                                .font(.largeTitle)
-                                .frame(width: 48, height: 48)
-                        }
-                        .buttonStyle(.bordered)
-                        Spacer()
-                    }
-                    HStack(alignment:.center) {
-                        Button(action: {
-                            bleManager.writeOutgoingValue(data: "ML")
-                        }) {
-                            Image(systemName: "arrowtriangle.left.fill")
-                                .font(.largeTitle)
-                                .frame(width: 48, height: 48)
-                        }
-                        .buttonStyle(.bordered)
-                        //Spacer()
-                        Button(action: {
-                        }) {
-                            Image(systemName: "")
-                                .font(.largeTitle)
-                                .frame(width: 48, height: 48)
-                            
-                        }
-                        .buttonStyle(.bordered)
-                        .hidden()
-                        Button(action: {
-                            bleManager.writeOutgoingValue(data: "MR")
-                        }) {
-                            Image(systemName: "arrowtriangle.right.fill")
-                                .font(.largeTitle)
-                                .frame(width: 48, height: 48)
-                        }
-                        .buttonStyle(.bordered)
-                    }
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            bleManager.writeOutgoingValue(data: "MB")
-                        }) {
-                            Image(systemName: "arrowtriangle.down.fill")
-                                .font(.largeTitle)
-                                .frame(width: 48, height: 48)
-                            
-                        }
-                        .buttonStyle(.bordered)
-                        Spacer()
-                    }
-                    Spacer()
-                }.padding()
-            }
+            .navigationTitle("Bluetooth Remote")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationTitle("Bluetooth Remote")
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
