5a26e7f3ef92ab7fa3446cd23748eb7970e39fe2
runil <57109651+runil@users.noreply.github.com>
2022-05-10 09:52:38 +0200

    Added calls for fetching mowers and sessions

--- Mobile/Models/ApiModel.swift
+++ Mobile/Models/ApiModel.swift
@@ -9,8 +9,11 @@ import Foundation
 import Combine
 import SwiftUI
 
-class API: ObservableObject {
-    @Published var mower: [Mower] = []
+class API: NSObject, ObservableObject {
+    @Published var mowers = [Mower]()
+    @Published var mowingSessions = [MowingSession]()
+    @Published var obstacles = [Obstacle]()
+    @Published var serverAddress = "http://ec2-54-227-56-79.compute-1.amazonaws.com:8080"
     
     func getMower() {
         // Fetches mower
@@ -18,9 +21,45 @@ class API: ObservableObject {
         URLSession.shared.dataTask(with: url) { (data, _, _) in
             guard let data = data else { return }
             let decodedData = try! JSONDecoder().decode([Mower].self, from: data)
-
             DispatchQueue.main.async {
-                self.mower = decodedData
+                self.mowers = decodedData
+            }
+        }.resume()
+    }
+    
+    
+    func getMowers() {
+        guard let url = URL(string:  "http://ec2-54-227-56-79.compute-1.amazonaws.com:8080/mobile/user/mowers" ) else { return }
+        var request = URLRequest(url: url)
+        // request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+        request.setValue("Bearer xxx", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { ( data, response, error ) in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async {
+                do {
+                    self.mowers = try JSONDecoder().decode([Mower].self, from: data)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
+    
+    func getMowingSessions() {
+        guard let url = URL(string:  "http://ec2-54-227-56-79.compute-1.amazonaws.com:8080/mobile/mowingSessions/mobile" ) else { return }
+        var request = URLRequest(url: url)
+        // request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+        request.setValue("Bearer xxx", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { ( data, response, error ) in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async {
+                do {
+                    self.mowingSessions = try JSONDecoder().decode([MowingSession].self, from: data)
+                } catch {
+                    print(error)
+                }
             }
         }.resume()
     }
@@ -30,13 +69,36 @@ struct MowerPositions: Decodable, Equatable {
     var points: [[CGFloat]]
 }
 
+struct ObstaclePosition: Decodable, Equatable {
+    var points: [[CGFloat]]
+}
+
 struct Mower: Decodable, Identifiable {
-    var positionsId: Int
-    var mowerPositions: MowerPositions
-    var createdAt: String
-    var mowerId: Int
+    let mowerID, userID, status: String
+    var id: String {
+        mowerID
+    }
+}
+
+struct Obstacle: Decodable, Identifiable {
+    var obstacleId: Int
+    var userId: String
+    var imageClassification: String
+    var obstaclePosition: ObstaclePosition
+    var imagePath: String
+    var mowingSessionId: Int
     
     var id: Int {
-        positionsId
+        obstacleId
+    }
+}
+
+struct MowingSession: Decodable, Identifiable {
+    let mowingSessionId: Int
+    let mowerPositions: MowerPositions
+    let createdAt: String
+    let mowerId: Int
+    var id:Int {
+        mowingSessionId
     }
 }
