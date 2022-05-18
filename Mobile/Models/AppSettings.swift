6a568b861073a5efa38dac4fdc938ef579e3e4ef
runil <57109651+runil@users.noreply.github.com>
2022-05-18 19:29:20 +0200

    feat(navigation-improvement): Added possibility to toggle between mower modes.

--- Mobile/Models/AppSettings.swift
+++ Mobile/Models/AppSettings.swift
@@ -9,8 +9,9 @@ import Foundation
 class AppSettings: ObservableObject {
     @Published var username: String?
     @Published var authToken: String?
-    @Published var mowerIsOn: Bool
-    @Published var mowerIsBle: Bool
+    @Published var mowerAuto: Bool
+    @Published var mowerBle: Bool
+    @Published var mowerStop: Bool
     @Published var isSignedIn: Bool
     @Published var loginAttemptStatusMessage: String
 
@@ -18,8 +19,9 @@ class AppSettings: ObservableObject {
         self.username = nil
         self.authToken = nil
         self.isSignedIn = false
-        self.mowerIsOn = false
-        self.mowerIsBle = false
+        self.mowerAuto = false
+        self.mowerStop = false
+        self.mowerBle = false
         self.loginAttemptStatusMessage = ""
     }
 }
